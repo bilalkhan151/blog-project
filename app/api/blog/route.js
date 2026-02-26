d756df885c4ccb5b88359761d1aae57acbf81af5
bilalkhan151 <fa23bscs0081@maju.edu.pk>
2026-02-27 01:31:39 +0500

    add Api

---
+++ app/api/blog/route.js
@@ -0,0 +1,52 @@
+import {ConnectDB} from "@/lib/config/db"
+import { writeFile } from "fs/promises";
+import { title } from "process";
+import bmodel from "../../../lib/models/bmodel";
+const{NextResponse } = require("next/server")
+
+
+const LoadDB = async ()=>{
+    await ConnectDB();
+    
+}
+
+LoadDB();
+
+export async function GET(request) {
+    
+    return NextResponse.json({ msg: "Api is working"})
+    
+}
+
+
+export async function POST(request) {
+    const formData = await request.formData();
+    const timestamp = Date.now();
+
+
+    const image=formData.get('image');
+
+    const imagbyteData = await image.arrayBuffer()
+
+    const buffer= Buffer.from(imagbyteData);
+    const path =`./public/${timestamp}_${image.name}`;
+
+    await writeFile(path,buffer);
+    const imgUrl = `/${timestamp}_${image.name}`;
+
+    const blogData ={
+        title:`${formData.get('title')}`,
+        description:`${formData.get(description)}`,
+        category:`${formData.get('category')}`,
+        author:`${formData.get('author')}`,
+        image:`${imgUrl}`,
+        authorImg:`${formData.get('authorImg')}`
+
+
+    }
+
+    await bmodel.create(blogData);
+    console.log("Blog  saved")
+    return NextResponse.json({success:true,msg:"Blog Added"})
+    
+}
