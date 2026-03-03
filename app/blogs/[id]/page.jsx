75873b1a205641b8a4684f603bcf14f3aa32c629
bilalkhan151 <fa23bscs0081@maju.edu.pk>
2026-03-03 22:12:53 +0500

    Fixed assets imports for netlify

--- app/blogs/[id]/page.jsx
+++ app/blogs/[id]/page.jsx
@@ -1,5 +1,5 @@
 "use client"
-import { assets } from '@/Assets/assets';
+import { assets } from '../Assets/assets';
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
